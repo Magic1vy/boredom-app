f3d08e7692aff37b89e7786f91f8cc5d2c1ba0bb
Valeriia Isaienko <lerika@MacBook-Pro.local>
2023-02-14 18:59:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [advice, setAdvice] = useState("")
+  const [image, setImage] = useState("")
+
+  useEffect ( ()=> {
+    const getResponse = async () => {
+    const responce = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await responce.json();
+    setAdvice(data.activity);
+    }
+    getResponse()
+  },[])
+
+  const changeAdvice = async () => {
+    const responce = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await responce.json();
+    setAdvice(data.activity);
+  }
+
+    const accessKey = 'Ih9cQX5igRDGvADWU3WrRKBA6LdB0GltAB4r3bNBpVs';
+    const orientation = 'landscape';
+    const query = 'dark';
+
+  useEffect ( ()=> {
+    const getRandomPhoto = async () => {
+    const response = await fetch(`https://api.unsplash.com/photos/random?orientation=${orientation}&query=${query}`, {
+      headers: {
+        'Authorization': `Client-ID ${accessKey}`
+      }
+    });
+    const data = await response.json();
+    setImage(data.urls.regular);
+    console.log(data.urls.regular);
+  
+}
+getRandomPhoto();
+
+  },[])
+
+  const changeImage = async () => {
+    const response = await fetch(`https://api.unsplash.com/photos/random?orientation=${orientation}&query=${query}`, {
+      headers: {
+        'Authorization': `Client-ID ${accessKey}`
+      }
+    });
+    const data = await response.json();
+    setImage(data.urls.regular);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img className='picture' src={image} alt="random"/>
+      </div>
+      
+<div >
+<h1>If you do not know what to do, read advice bellow:</h1>
+</div>
+<div>
+  <h2>{advice}</h2>
+</div>
+<div>
+  <button className='btn' onClick={event => {
+          			changeAdvice();
+          			changeImage();
+        		}}>Refresh Advice</button>
+</div>
     </div>
   );
 }
